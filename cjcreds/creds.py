79cf5435d024530f0f01dce79fe4b8ed0b94c131
Mumit Prottoy <mumitprottoy@gmail.com>
2025-05-20 14:34:33 +0600

    dumped db data in data.json

--- cjcreds/creds.py
+++ cjcreds/creds.py
@@ -1,7 +1,7 @@
 PROJECT_SECRET_KET = 'django-insecure-=$w7mvsybo@-24ej1t9hubumim!pfk_$hs=fa3e1bkx)%0bmw!'
 DB_NAME = 'clapjam'
-DB_HOST = '127.0.0.1',
+DB_HOST = '127.0.0.1'
 DB_PORT = '3306'
 DB_USER = 'root'
 DB_PASSWORD = 'make_strong_password(mysql)'
-DB_OPTIONS = {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" , 'charset': 'utf8mb4'},
+DB_OPTIONS = {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" , 'charset': 'utf8mb4'}
