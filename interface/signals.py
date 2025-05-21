b2750f03ef081fc332eabd6a13de84a32f4900c2
Mumit Prottoy <mumitprottoy@gmail.com>
2025-05-21 08:43:17 +0600

    'added auto unique username feature'

--- interface/signals.py
+++ interface/signals.py
@@ -1,8 +1,11 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from profiles import operations as profile_ops
 
 
 @receiver(pre_save, sender=User)
-def add_unique_username(instance: User, created: bool, *args, **kwargs) -> None:
-    pass
+def add_unique_username(instance: User, *args, **kwargs) -> None:
+    user = instance
+    user.username = profile_ops.create_unique_username_from_fullname(
+        f"{user.first_name} {user.last_name}")
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,2 @@
 from django.test import TestCase
-
-# Create your tests here.
+from . import operations as ops, models
